46ca6427df1f9d4f98a9b377f0e52795c713ed05
Hansani996 <hansanimadushika.996@gmail.com>
2022-02-02 09:36:33 +0530

    initial commit

---
+++ src/test/java/com/bolton/HappyPetCare/HappyPetCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolton.HappyPetCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyPetCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
